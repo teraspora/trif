b4d0578e084443748b381f1ec01f5ac42c3bfff1
John Lynch <johnlynch.32768@gmail.com>
2019-05-13 21:28:21 +0100

    Fix errors and typos

--- trif/settings.py
+++ trif/settings.py
@@ -45,7 +45,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'fract.apps.FractConfig',
+    'users.apps.UsersConfig',
     'storages',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
