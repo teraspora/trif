61e76f8ab1301871676e28dc757e6064f2c01939
John Lynch <johnlynch.32768@gmail.com>
2019-05-14 19:32:37 +0100

    Add code to pull image names from database and display corresponding images from S3

--- fract/views.py
+++ fract/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
-
-# Create your views here.
-from django.http import HttpResponse
-
+from models import Image
 
 def index(request):
-    return render(request, 'fract/index.html')
+    image_names = map(lambda img: img.name, Image.objects.filter(id__lt=13))
+    context = {
+        'image_names': image_names
+    }
+    return render(request, 'fract/index.html', context)
