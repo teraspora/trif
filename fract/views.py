35f7f6d3f995d9f3bb095ce3a3d5d3a84f022d00
John Lynch <johnlynch.32768@gmail.com>
2019-05-14 21:43:47 +0100

    Make home page display all 438x310 ('SMALL') images referenced in database

--- fract/views.py
+++ fract/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 from .models import Image
-    
+
 STATIC_IMAGE_DIR = 'images/zarg438/'
 
 def index(request):
-    image_names = map(lambda img: STATIC_IMAGE_DIR + img.name, Image.objects.filter(id__lt=13))
+    #image_names = map(lambda img: STATIC_IMAGE_DIR + img.name, Image.objects.filter(id__lt=13))
+    image_names = map(lambda img: STATIC_IMAGE_DIR + img.name, Image.objects.all())
     context = {
         'image_names': image_names
     }
