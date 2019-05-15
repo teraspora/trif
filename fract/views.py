4788eca125f74e0bd21d07b64cc75faadd3f3d82
John Lynch <johnlynch.32768@gmail.com>
2019-05-15 03:41:40 +0100

    Pass and display id numbers for each image

--- fract/views.py
+++ fract/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
 from .models import Image
-import fract.populate_db
+# import fract.populate_db # ------- only run this to initially populate database with Image objects
 STATIC_IMAGE_DIR = 'images/zarg438/'
 
 def index(request):
-    #image_names = map(lambda img: STATIC_IMAGE_DIR + img.name, Image.objects.filter(id__lt=13))
-    image_names = map(lambda img: STATIC_IMAGE_DIR + img.name, Image.objects.all())
+    #image_names = map(lambda img: STATIC_IMAGE_DIR + img.name, Image.objects.filter(id__lt=13)) # for testing
+
+    # Get image data as a list of tuples like ('xtsM2f101C431-pre167-438x310x0.0y0.0_6594.png', 74)
+    image_data = map(lambda img: (STATIC_IMAGE_DIR + img.name, img.id), Image.objects.all())
     context = {
-        'image_names': image_names
+        'image_data': image_data
     }
     return render(request, 'fract/index.html', context)
