3c8694f174861b035ce60aef869b4dc5df660310
John Lynch <johnlynch.32768@gmail.com>
2019-05-16 03:31:59 +0100

    Add code to allow user to update her/his profile and upload a profile picture

--- users/views.py
+++ users/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
-from .forms import UserRegisterForm
+from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
 from django.contrib.auth.decorators import login_required
 
  # Create your views here.
@@ -19,4 +19,25 @@ def register(request):
 
 @login_required
 def profile(request):
-    return render(request, 'users/profile.html')
+    if request.method == 'POST':
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        p_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
+        if u_form.is_valid and p_form.is_valid:
+            u_form.save()
+            p_form.save()
+            username = u_form.cleaned_data.get('username')
+            messages.success(request, f'Account updated: {username}.')
+            # Serve the profile page by GET request to obviate duplicate POST data
+            return redirect('profile') 
+
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm(instance=request.user.profile)
+
+
+
+    context = {
+        'u_form': u_form,
+        'p_form': p_form
+    }
+    return render(request, 'users/profile.html', context)
