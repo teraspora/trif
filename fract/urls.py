b58d3644bc3eedca946b5f17f8e7a27a5fd8028a
John Lynch <johnlynch.32768@gmail.com>
2019-05-16 20:14:15 +0100

    Change home page to class-based view, subclassing ListView

--- fract/urls.py
+++ fract/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-
+from .views import ImageListView
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    # path('', views.index, name='index'),
+    path('', ImageListView.as_view(), name='index'),
 ]
--- fract/views.py
+++ fract/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 from .models import Image
 
 # ------- only run this to initially populate database with Image objects
@@ -7,12 +8,24 @@ if len(Image.objects.all()) == 0:
 
 STATIC_IMAGE_DIR = 'images/zarg438/'
 
-def index(request):
-    #image_names = map(lambda img: STATIC_IMAGE_DIR + img.name, Image.objects.filter(id__lt=13)) # for testing
+# OLD view-function, replaced by class-based view below
+# def index(request):
+#     #image_names = map(lambda img: STATIC_IMAGE_DIR + img.name, Image.objects.filter(id__lt=13)) # for testing
 
-    # Get image data as a list of tuples like ('xtsM2f101C431-pre167-438x310x0.0y0.0_6594.png', 74)
-    image_data = map(lambda img: (STATIC_IMAGE_DIR + img.name, img.id), Image.objects.all())
-    context = {
-        'image_data': image_data
-    }
-    return render(request, 'fract/index.html', context)
+#     # Get image data as a list of tuples like ('xtsM2f101C431-pre167-438x310x0.0y0.0_6594.png', 74)
+#     image_data = map(lambda img: (STATIC_IMAGE_DIR + img.name, img.id), Image.objects.all())
+#     context = {
+#         'image_data': image_data
+#     }
+#     return render(request, 'fract/index.html', context)
+
+class ImageListView(ListView):
+    model = Image
+    template_name = 'fract/index.html'
+    context_object_name = 'image_list'
+
+    def get_context_data(self, **kwargs):
+        context = super(ImageListView, self).get_context_data(**kwargs)
+        context['STATIC_IMAGE_DIR'] = STATIC_IMAGE_DIR
+        return context
+
