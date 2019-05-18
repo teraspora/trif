fcc757650d9eba729cbdaaada9dd9ce9744a509d
John Lynch <johnlynch.32768@gmail.com>
2019-05-18 02:48:22 +0100

    Add documentation to add_images.py

--- fract/add_images.py
+++ fract/add_images.py
@@ -1,3 +1,23 @@
+# add_images.py
+
+# Run in the shell: input the directory of SMALL images you want to add to the database.
+# Don't forget to add the actual images, and their LARGE counterparts, to the S3 bucket.
+# This process is a candidate for further automation...
+
+# Example:
+
+# 02:25: fractrig $ python3 manage.py shell
+# In DEVELOPMENT context
+# Python 3.7.1 (default, Oct 22 2018, 11:21:55) 
+# [GCC 8.2.0] on linux
+# Type "help", "copyright", "credits" or "license" for more information.
+# (InteractiveConsole)
+# >>> from fract import add_images
+# Enter directory string: fract/test
+# Directory: fract/test.   Continue? y/ny
+# xtsJ1f353C401-pre60-sri-438x310x-3.036256350301346y-1.4303025782411303_5841.png saved.
+# Done.   1 image names saved in the database.
+
 from .models import Image, SMALL, LARGE
 import os
 
