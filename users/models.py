f93238ff37ac7dd3beb316b20a4bccbdfd76e77f
John Lynch <johnlynch.32768@gmail.com>
2019-05-20 00:16:37 +0100

    Revert code to before profile image resize attempt; comment custom_storages.py

--- users/models.py
+++ users/models.py
@@ -3,7 +3,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from PIL import Image
-from resizeimage import resizeimage
+# from resizeimage import resizeimage
 
 class Profile(models.Model):
     # CASCADE => if user is deleted, then user's profile is also deleted
