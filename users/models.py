122575bccfeb24afe1cf06bc4a323b68d8609501
John Lynch <johnlynch.32768@gmail.com>
2019-05-20 16:30:14 +0100

    Add necessary import to users/models.py

--- users/models.py
+++ users/models.py
@@ -2,15 +2,16 @@
 
 from django.db import models
 from django.contrib.auth.models import User
-from PIL import Image
+# from PIL import Image
 # from resizeimage import resizeimage
+from fract.models import Image
 
 class Profile(models.Model):
     # CASCADE => if user is deleted, then user's profile is also deleted
     # OneToOneField => user has same structure as User model from django.contrib.auth.models
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = models.ImageField(default='default.jpg', upload_to='profile_pics')
-    liked_images = models.ManyToManyField(fract.Image)
+    liked_images = models.ManyToManyField(Image)
 
     def __str__(self):
         return f'{self.user.username}\'s profile'
