e1ff6373fab5f43d9b9b939ea2583b1473db2dee
John Lynch <johnlynch.32768@gmail.com>
2019-05-27 17:07:26 +0100

    Fix indentation in

--- fract/context_processors.py
+++ fract/context_processors.py
@@ -1,8 +1,9 @@
-# context_processors.py
+# fract/context_processors.py
+
+# Code adapted from an answer here:
+# https://stackoverflow.com/questions/6166055/show-a-form-in-my-template-base-using-django
 
 from .forms import ImageFilterForm
 
 def filter_form(request):
-return {
-     'filter_form' : ImageFilterForm()
-}
+    return { 'filter_form' : ImageFilterForm() }
