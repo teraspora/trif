61b5325f796bd50a563839079a31525f86a4589e
John Lynch <johnlynch.32768@gmail.com>
2019-05-27 19:07:47 +0100

    Add initial value to flavour ChoiceField in ImageFilterForm

--- fract/forms.py
+++ fract/forms.py
@@ -5,7 +5,7 @@ from .models import Image
 
 class ImageFilterForm(forms.Form):
     all_imgs = Image.objects.all()
-    FLAVOURS = (('M', 'Mandelbrot'), ('J', 'Julia'))
+    FLAVOURS = (('M', 'Mandelbrot'), ('J', 'Julia'), ('*', 'Both'))
     power_max = max([int(img.params()['power']) for img in all_imgs])
     f_min = min([int(img.params()['func']) for img in all_imgs])
     f_max = max([int(img.params()['func']) for img in all_imgs])
@@ -15,5 +15,5 @@ class ImageFilterForm(forms.Form):
     func = forms.IntegerField(label='Function', min_value=f_min, max_value=f_max, required=False)
     alt_func = forms.IntegerField(label='Alt-function', min_value=g_min, max_value=g_max, required=False)   
     exponent = forms.IntegerField(label='Exponent', min_value=1, max_value=power_max, required=False)
-    flavour = forms.ChoiceField(label='Type', choices=FLAVOURS, required=False)
-    
+    flavour = forms.ChoiceField(label='Type', choices=FLAVOURS, initial='*', required=False)
+
