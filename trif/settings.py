04a699f984fcb858f9d161865de3ee74ea0628af
John Lynch <johnlynch.32768@gmail.com>
2019-05-27 23:29:18 +0100

    Add 'AWS_DEFAULT_ACL = None' to settings.py, to eliminate boto3 warning re default ACL in django-storages 2.0

--- trif/settings.py
+++ trif/settings.py
@@ -149,7 +149,7 @@ AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
 
 AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
-
+AWS_DEFAULT_ACL = None  # to eliminate boto3 warning
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
