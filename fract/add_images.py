7f90194e7f7f35f29d885fb4aff88ac7bcb19cc1
John Lynch <johnlynch.32768@gmail.com>
2019-05-30 21:46:30 +0100

    Insert missing space in assignment

--- fract/add_images.py
+++ fract/add_images.py
@@ -29,7 +29,7 @@ if input(f'Directory: {directory}.   Continue? y/n') == 'y':
     i = 0
     for name in os.listdir(directory):
         if name.startswith(PREFIX) and name.endswith(SUFFIX) and SMALL in name:
-            img =Image(name=name)
+            img = Image(name=name)
             img.save()
             i += 1
             print(f'{name} saved.')
