1835f92419d43e65bd5e893c5d5efebbaea3b24c
John Lynch <johnlynch.32768@gmail.com>
2019-05-31 02:50:22 +0100

    Add missing '/' to likes URL

--- fract/urls.py
+++ fract/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('', ImageListView.as_view(), name='index'),
     path('image/<int:pk>/', ImageDetailView.as_view(), name='image'),
     path('add_like/<int:img_id>/', user_views.add_like, name='add_like'),
-    path('likes', LikedImageListView.as_view(), name='likes'),
+    path('likes/', LikedImageListView.as_view(), name='likes'),
     path('about/', views.about, name='about'),
     path('images_filtered/', FilteredImageListView.as_view(), name='images_filtered'),        
 ]   
